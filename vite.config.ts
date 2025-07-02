31b6465612874297acb6d780d6822ac5e173263b
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-07-02 14:09:16 +0600

    vite config :tada:

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 });
-
+// Vite Config
 // import { defineConfig } from 'vite';
 // import reactRefresh from '@vitejs/plugin-react-refresh';
 // import dotenv from 'dotenv';
